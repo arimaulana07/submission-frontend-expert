a3f1feca9f65355c24422930b001a4739cc89a0a
Ari Said Maulana <ariisaiid@gmail.com>
2023-01-15 22:56:01 +0700

    add header, hero, and hardcoded restaurant list

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
-
+import '../styles/main.css'; 
+import "@fortawesome/fontawesome-free/css/all.css";
 console.log('Hello Coders! :)');
