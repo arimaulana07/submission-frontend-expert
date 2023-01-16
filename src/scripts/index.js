22218019fc6e832e733bb9dd4fa830bd5a6db9bb
Ari Said Maulana <ariisaiid@gmail.com>
2023-01-16 11:14:43 +0700

    add navbar, popup menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,21 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css'; 
 import "@fortawesome/fontawesome-free/css/all.css";
 console.log('Hello Coders! :)');
+
+const openNavBtn = document.querySelector('#openNav');
+const closeNavBtn = document.querySelector('#closeNav');
+const navMenu = document.querySelector('.navMenu');
+const mainElement = document.querySelector('main');
+
+openNavBtn.addEventListener('click', () => {
+  navMenu.classList.add('open')
+})
+
+closeNavBtn.addEventListener('click', () => {
+  navMenu.classList.remove('open')
+})
+
+mainElement.addEventListener('click', event => {
+  navMenu.classList.remove('open');
+  event.stopPropagation();
+});
